5d1833d8e38bc991dcc106abe63534197c6edc09
bgby <briac.grauby@etu.upmc.fr>
2019-05-09 18:37:17 +0200

    Modification de Plateau.hpp et Plateau.cpp

--- test/Plateau.cpp
+++ test/Plateau.cpp
@@ -1,6 +1,11 @@
 #include "Plateau.hpp"
 
 Plateau::Plateau(){
-	set("monopolyPlateau900.jpg");
-	show_all();
+	//set("monopolyPlateau900.jpg");
+	
+	//show_all();
+
+	//image = Gdk::Pixbuf::create_from_file("monopolyPlateau900.png");
 }
+
+//Plateau::~Plateau(){}
--- test/Plateau.hpp
+++ test/Plateau.hpp
@@ -1,12 +1,22 @@
 #ifndef PLATEAU
 #define PLATEAU
 
-#include <gtkmm/image.h>
+#include <gtkmm/drawingarea.h>
+#include <iostream>
+//#include <cairomm>
 
-class Plateau : public Gtk::Image{
+class Plateau : public Gtk::DrawingArea{
 	public:
 		Plateau();
+		//~Plateau();
+/*
+	protected:
+		//Override default signal handler:
+		virtual bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr);
 
+	private:
+		Glib::RefPtr<Gdk::Pixbuf> image;
+*/
 };
 
 #endif
